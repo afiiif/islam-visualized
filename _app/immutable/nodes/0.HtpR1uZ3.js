9a89ab6918954a6c904563b6e11ec5765f9a81c4
M. Afifudin <afif.x.udin@gmail.com>
2025-07-21 16:31:59 +0700

    just prototype

---
+++ _app/immutable/nodes/0.HtpR1uZ3.js
@@ -0,0 +1,1 @@
+import"../chunks/DsnmJJEf.js";import{P as n,Q as a,R as p}from"../chunks/7Io1OPw0.js";import{s}from"../chunks/2x6V3zYS.js";const i=!0,u=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function d(o,r){var e=n(),t=a(e);s(t,()=>r.children),p(o,e)}export{d as component,u as universal};
